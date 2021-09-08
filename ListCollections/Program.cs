49394c8f4badce694e44af39c91a6ebca143eac8
andrzej.palica <andrzej.palica@shijigroup.com>
2021-09-08 22:51:29 +0200

    added solutions for exercies from List collections section

--- ListCollections/Program.cs
+++ ListCollections/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ListCollections
 {
@@ -6,7 +7,85 @@ namespace ListCollections
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //WorkWithStrings();
+
+            var fibonacciNumbers = new List<int> { 1, 1 };
+
+            for (int i = 3; i <= 20; i++)
+            {
+                var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+                var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+
+                fibonacciNumbers.Add(previous + previous2);
+            }
+                
+            foreach (var item in fibonacciNumbers)
+            {
+                Console.WriteLine(item);
+            }
+            
+            void WorkWithStrings()
+            {
+                var names = new List<string> { "Andrzej", "Ana", "Felipe" };
+
+                foreach (var name in names)
+                {
+                    Console.WriteLine($"Hello {name.ToUpper()}!");
+                }
+
+                Console.WriteLine();
+
+                names.Add("Maria");
+                names.Add("Bill");
+                names.Remove("Ana");
+
+                foreach (var name in names)
+                {
+                    Console.WriteLine($"Hello {name.ToUpper()}!");
+                }
+
+                Console.WriteLine();
+
+                Console.WriteLine($"My name is {names[0]}.");
+                Console.WriteLine($"I've added {names[2]} and {names[3]} to the list.");
+
+                Console.WriteLine();
+
+                Console.WriteLine($"The list has {names.Count} people in it.");
+
+                Console.WriteLine();
+
+                var index = names.IndexOf("Felipe");
+
+                if (index == -1)
+                {
+                    Console.WriteLine($"When an item is not found, IndexOf returns {index}");
+                }
+                else
+                {
+                    Console.WriteLine($"The name {names[index]} is an index {index}");
+                }
+
+                index = names.IndexOf("Not Found");
+
+                if (index == -1)
+                {
+                    Console.WriteLine($"When an item is not found, IndexOf returns {index}");
+                }
+                else
+                {
+                    Console.WriteLine($"The name {names[index]} is an index {index}");
+                }
+
+                Console.WriteLine();
+
+                names.Sort();
+
+                foreach (var name in names)
+                {
+                    Console.WriteLine($"Hello {name.ToUpper()}!");
+                }
+            }
         }
     }
 }
